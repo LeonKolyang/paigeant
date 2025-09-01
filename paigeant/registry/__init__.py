9f16f65b454865a89ab11de9c5275452455896fe
Leon Menkreo <50681264+LeonKolyang@users.noreply.github.com>
2025-09-01 01:06:54 -0600

    feat: add registry data models

---
+++ paigeant/registry/__init__.py
@@ -0,0 +1,17 @@
+"""Registry data models and utilities."""
+
+from .models import (
+    SemanticVersion,
+    ParamDescriptor,
+    AgentDescriptor,
+    Group,
+    RegistryRoot,
+)
+
+__all__ = [
+    "SemanticVersion",
+    "ParamDescriptor",
+    "AgentDescriptor",
+    "Group",
+    "RegistryRoot",
+]
