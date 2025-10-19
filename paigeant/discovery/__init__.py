b4e2e3296aa3f4cf40d9cb5f544e4456c1dfbf0f
Leon Menkreo <leon.menkreo@googlemail.com>
2025-10-18 20:35:00 -0600

    feat: add models for worklfows and agents

---
+++ paigeant/discovery/__init__.py
@@ -0,0 +1,21 @@
+"""Discovery domain models and helpers."""
+
+from .entities import (
+    AgentDefinition,
+    DependencyDefinition,
+    DiscoverySource,
+    SourcePosition,
+    SourceSpan,
+    WorkflowAgentRef,
+    WorkflowDefinition,
+)
+
+__all__ = [
+    "AgentDefinition",
+    "DependencyDefinition",
+    "DiscoverySource",
+    "SourcePosition",
+    "SourceSpan",
+    "WorkflowAgentRef",
+    "WorkflowDefinition",
+]
